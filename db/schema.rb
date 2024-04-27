34d15961e5e302d7064ed9412c111a49249fc551
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-27 17:46:27 -0300

    Rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_27_142635) do
-  create_table "buffets", force: :cascade do |t|
-    t.string "brand_name"
-    t.string "corporate_name"
-    t.string "registration_number"
-    t.string "phone"
-    t.string "email"
-    t.string "address"
-    t.string "string"
-    t.string "city"
-    t.string "state"
-    t.string "zip_code"
-    t.string "description"
-    t.string "payment"
-    t.integer "owner_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["owner_id"], name: "index_buffets_on_owner_id"
+ActiveRecord::Schema[7.1].define(version: 20_240_427_142_635) do
+  create_table 'buffets', force: :cascade do |t|
+    t.string 'brand_name'
+    t.string 'corporate_name'
+    t.string 'registration_number'
+    t.string 'phone'
+    t.string 'email'
+    t.string 'address'
+    t.string 'string'
+    t.string 'city'
+    t.string 'state'
+    t.string 'zip_code'
+    t.string 'description'
+    t.string 'payment'
+    t.integer 'owner_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['owner_id'], name: 'index_buffets_on_owner_id'
   end
 
-  create_table "owners", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_owners_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
+  create_table 'owners', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_owners_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_owners_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "buffets", "owners"
+  add_foreign_key 'buffets', 'owners'
 end
