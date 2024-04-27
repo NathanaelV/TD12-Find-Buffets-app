14d663fc53e491aee2e9d8cd8b66d5988067c616
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-27 09:02:01 -0300

    Buffet owner logs in

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Owner.find_or_create_by!(name: 'Leonardo Yoshi', email: 'leonardo.yoshi@email.com',
+                               encrypted_password: 'leonardo123')
+Owner.find_or_create_by!(name: 'Splinter Yoshi', email: 'splinter.yoshi@email.com',
+                               encrypted_password: 'splinter123')
+
+# Buffet
+Buffet.find_or_create_by!(brand_name: 'Teenage Mutant Ninja Turtles', corporate_name: 'TMNT Splinter LTDA',
+                          registration_number: '88392017000182', phone: '11912341234',
+                          email: 'contato@tmntsplinter.com', address: 'Rua Estados Unidos, 1030 - Jardins',
+                          city: 'São Paulo', state: 'SP', zip_code: '01234123',
+                          description: 'Melhor Buffet da região. Cowabunga', payment_id: Payment.first.id,
+                          buffet_owner_id: BuffetOwner.first.id)
+
+# Event
+Event.find_or_create_by!(name: 'Festa infantil', description: 'Festa para crianças com temática TMNT',
+                         min_people: 10, max_people: 100, duration: 300, menu: 'Pizza', alcoholic_beverages: true,
+                         decoration: true, parking: true, parking_valet: true, customer_space: true,
+                         buffet_id: Buffet.first.id)
---
+++ spec/system/authentication/owner_sign_in_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Buffet owner sing in' do
+  it 'successfully, but without Buffet registered' do
+    Owner.create!(name: 'Leonardo', email: 'leonardo@email.com', password: 'password')
+
+    visit root_path
+    click_on 'Login'
+    within 'form' do
+      fill_in 'E-mail',	with: 'leonardo@email.com'
+      fill_in 'Senha', with: 'password'
+      click_on 'Login'
+    end
+
+    within('nav') do
+      expect(page).to have_content 'Leonardo <leonardo@email.com>'
+      expect(page).to have_button 'Sair'
+    end
+    expect(page).to have_content 'Login efetuado com sucesso'
+    expect(page).not_to have_link 'Login'
+  end
+end
