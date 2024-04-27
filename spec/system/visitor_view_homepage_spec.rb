542ada9f0e710843e10e1e26d8e746bfc14c23fb
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-27 09:05:40 -0300

    Buffet Owner logs out

--- spec/system/visitor_view_homepage_spec.rb
+++ spec/system/visitor_view_homepage_spec.rb
@@ -8,4 +8,14 @@ describe 'User view homepage' do
     expect(page).to have_link 'Buffets', href: root_path
     expect(page).not_to have_content 'Criar Evento'
   end
+
+  it 'view buffets registered' do
+    owner = Owner.create!(name: 'Splinter', email: 'splinter@email.com', password: 'password')
+    buffet = Buffet.create!(brand_name: 'TMNT Buffet', owner:)
+
+    visit root_path
+
+    expect(page).to have_content 'TMNT Buffet'
+    expect(page).to have_link 'TMNT Buffet', href: buffet_path(buffet)
+  end
 end
