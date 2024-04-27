27fdca81eee7ecb1aa0914dd618020836ddc7038
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-27 09:14:31 -0300

    Buffet owner logs out

--- spec/system/authentication/owner_sign_in_spec.rb
+++ spec/system/authentication/owner_sign_in_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Buffet owner sing in' do
   it 'successfully, but without Buffet registered' do
-    Owner.create!(name: 'Leonardo', email: 'leonardo@email.com', password: 'password')
+    BuffetOwner.create!(name: 'Leonardo', email: 'leonardo@email.com', password: 'password')
 
     visit root_path
     click_on 'Login'
@@ -18,5 +18,25 @@ describe 'Buffet owner sing in' do
     end
     expect(page).to have_content 'Login efetuado com sucesso'
     expect(page).not_to have_link 'Login'
+  end
+
+  it 'and logout' do
+    BuffetOwner.create!(name: 'Leonardo', email: 'leonardo@email.com', password: 'password')
+
+    visit root_path
+    click_on 'Login'
+    within 'form' do
+      fill_in 'E-mail',	with: 'leonardo@email.com'
+      fill_in 'Senha', with: 'password'
+      click_on 'Login'
+    end
+    click_on 'Sair'
+
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    expect(page).not_to have_button 'Sair'
+    expect(page).not_to have_content 'Leonardo <leonardo@email.com>'
+    within('nav') do
+      expect(page).to have_link 'Login'
+    end
   end
 end
