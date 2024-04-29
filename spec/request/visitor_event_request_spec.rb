5aced5ae1e977a100df6bd45f3a008f567277781
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-29 05:22:39 -0300

    [WIP] Visitor authorization

--- spec/request/visitor_event_request_spec.rb
+++ spec/request/visitor_event_request_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 describe 'Visitor event request' do
   context 'create' do
-    it 'without authorization' do
-      owner = Owner.create!(name: 'Splinter', email: 'splinter@email.com', password: 'password')
+    # xit 'without authorization' do
+    #   owner = Owner.create!(name: 'Splinter', email: 'splinter@email.com', password: 'password')
 
-      Buffet.create!(brand_name: 'TMNT Buffet', payment: 'PIX', owner:)
+    #   Buffet.create!(brand_name: 'TMNT Buffet', payment: 'PIX', owner:)
 
-      post(events_path, params: { event: { name: 'Troca via request' } })
+    #   post(events_path, params: { event: { name: 'Troca via request' } })
 
-      expect(response).to redirect_to root_path
-    end
+    #   expect(response).to redirect_to root_path
+    # end
+  end
+
+  context 'update' do
+    
+  end
+
+  context 'delete' do
+    
   end
 end
