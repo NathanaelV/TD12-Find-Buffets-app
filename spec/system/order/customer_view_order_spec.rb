971d8f324e0a2b9580a78d72e8ce6e35cc6efce4
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-05-05 20:36:54 -0300

    Test Order status view

--- spec/system/order/customer_view_order_spec.rb
+++ spec/system/order/customer_view_order_spec.rb
@@ -54,4 +54,60 @@ describe 'Customer view orders' do
 
     expect(page).to have_content 'Nenhum pedido cadastrado'
   end
+
+  it 'order is approved' do
+    owner = Owner.create!(name: 'Splinter', email: 'splinter@email.com', password: 'password')
+
+    buffet = Buffet.create!(brand_name: 'TMNT Buffet', payment: 'PIX', owner:)
+
+    event = Event.create!(name: 'Festa de casamento', description: 'Festa de casamento dos sonhos', min_people: 10,
+                          max_people: 100, duration: 420, menu: 'Pizza', alcoholic_beverages: true, decoration: true,
+                          parking: true, parking_valet: true, customer_space: true, buffet:)
+
+    EventCost.create!(description: 'Dias de semana', minimum: 2_000, additional_per_person: 70, overtime: 1000, event:)
+
+    customer = Customer.create!(name: 'Dragon Shiryu', cpf: '665.455.630-50', email: 'shiryu@email.com',
+                                password: 'shiryu123')
+
+    future_date = 2.week.from_now.strftime('%d/%m/%Y')
+    order = Order.create!(event_date: future_date, people: 80, details: 'Dia especial', address: 'Sítio ', buffet:,
+                          customer:, event:, status: :approved)
+                                
+    login_as customer, scope: :customer
+    visit root_path
+    within 'nav' do
+      click_on 'Meus Pedidos'
+    end
+    click_on "#{future_date} - #{order.code}"
+
+    expect(page).to have_content 'Status: Pedido Confirmado'
+  end
+
+  it 'order is canceled' do
+    owner = Owner.create!(name: 'Splinter', email: 'splinter@email.com', password: 'password')
+
+    buffet = Buffet.create!(brand_name: 'TMNT Buffet', payment: 'PIX', owner:)
+
+    event = Event.create!(name: 'Festa de casamento', description: 'Festa de casamento dos sonhos', min_people: 10,
+                          max_people: 100, duration: 420, menu: 'Pizza', alcoholic_beverages: true, decoration: true,
+                          parking: true, parking_valet: true, customer_space: true, buffet:)
+
+    EventCost.create!(description: 'Dias de semana', minimum: 2_000, additional_per_person: 70, overtime: 1000, event:)
+
+    customer = Customer.create!(name: 'Dragon Shiryu', cpf: '665.455.630-50', email: 'shiryu@email.com',
+                                password: 'shiryu123')
+
+    future_date = 2.week.from_now.strftime('%d/%m/%Y')
+    order = Order.create!(event_date: future_date, people: 80, details: 'Dia especial', address: 'Sítio ', buffet:,
+                          customer:, event:, status: :canceled)
+                                
+    login_as customer, scope: :customer
+    visit root_path
+    within 'nav' do
+      click_on 'Meus Pedidos'
+    end
+    click_on "#{future_date} - #{order.code}"
+
+    expect(page).to have_content 'Status: Pedido Cancelado'
+  end
 end
