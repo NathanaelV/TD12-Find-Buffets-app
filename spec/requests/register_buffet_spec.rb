44712721028c0e93555522c33982ce709391e806
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-05-19 07:51:53 -0300

    Rubocop

--- spec/requests/register_buffet_spec.rb
+++ spec/requests/register_buffet_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
 describe 'Register Buffet' do
-  xit "when owner already has a buffet" do
-    
+  xit 'when owner already has a buffet' do
   end
 
   xit 'customer cannot register' do
-    
   end
 
   xit 'visitor cannot register' do
-    
   end
 end
--- spec/requests/register_event_cost_spec.rb
+++ spec/requests/register_event_cost_spec.rb
@@ -2,10 +2,8 @@ require 'rails_helper'
 
 describe 'Register Event Cost' do
   xit 'customer cannot' do
-    
   end
 
   xit 'visitor cannot' do
-    
   end
 end
