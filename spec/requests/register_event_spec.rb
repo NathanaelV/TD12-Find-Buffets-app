2fe3868c7063234a13dbed1f1927d4a957b8281c
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-05-20 10:11:52 -0300

    Rubocop

--- spec/requests/register_event_spec.rb
+++ spec/requests/register_event_spec.rb
@@ -5,10 +5,10 @@ describe 'Register Event' do
     owner = Owner.create!(name: 'Splinter', email: 'splinter@email.com', password: 'password')
 
     Buffet.create!(brand_name: 'Teenage Mutant Ninja Turtles', corporate_name: 'TMNT Splinter LTDA',
-                    registration_number: '88392017000182', phone: '11912341234',
-                    email: 'contato@tmntsplinter.com', address: 'Rua Estados Unidos, 1030 - Jardins',
-                    city: 'São Paulo', state: 'SP', zip_code: '01234123',
-                    description: 'Melhor Buffet da região. Cowabunga', payment: 'PIX, Cartão de Débito', owner:)
+                   registration_number: '88392017000182', phone: '11912341234',
+                   email: 'contato@tmntsplinter.com', address: 'Rua Estados Unidos, 1030 - Jardins',
+                   city: 'São Paulo', state: 'SP', zip_code: '01234123',
+                   description: 'Melhor Buffet da região. Cowabunga', payment: 'PIX, Cartão de Débito', owner:)
 
     post(events_path, params: { event: { name: 'Troca via request' } })
 
