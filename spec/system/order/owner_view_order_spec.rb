8ad01f4ae7097c5288c6a868f243ba456b45ed78
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-05-21 06:21:10 -0300

    Add back button on Orders and Proposals

--- spec/system/order/owner_view_order_spec.rb
+++ spec/system/order/owner_view_order_spec.rb
@@ -127,4 +127,35 @@ describe 'Owner view order' do
 
   xit 'if it is yours' do
   end
+
+  it 'and click on back button' do
+    owner = Owner.create!(name: 'Splinter', email: 'splinter@email.com', password: 'password')
+
+    buffet = Buffet.create!(brand_name: 'Teenage Mutant Ninja Turtles', corporate_name: 'TMNT Splinter LTDA',
+                            registration_number: '88392017000182', phone: '11912341234',
+                            email: 'contato@tmntsplinter.com', address: 'Rua Estados Unidos, 1030 - Jardins',
+                            city: 'São Paulo', state: 'SP', zip_code: '01234123',
+                            description: 'Melhor Buffet da região. Cowabunga', payment: 'PIX, Cartão de Débito', owner:)
+
+    event = Event.create!(name: 'Festa de casamento', description: 'Festa de casamento dos sonhos', min_people: 10,
+                          max_people: 100, duration: 420, menu: 'Pizza', alcoholic_beverages: true, decoration: true,
+                          parking: true, parking_valet: true, customer_space: true, buffet:)
+
+    customer = Customer.create!(name: 'Dragon Shiryu', cpf: '665.455.630-50', email: 'shiryu@email.com',
+                                password: 'shiryu123')
+
+    future_date = 1.week.from_now.strftime('%d/%m/%Y')
+    order = Order.create!(event_date: future_date, people: 80, details: 'Dia especial', address: 'Sítio', buffet:,
+                          customer:, event:)
+
+    login_as owner, scope: :owner
+    visit root_path
+    within 'nav' do
+      click_on 'Pedidos'
+    end
+    click_on "#{future_date} - #{order.code}"
+    click_on 'Voltar para Pedidos'
+
+    expect(current_path).to eq orders_path
+  end
 end
--- spec/system/proposal/owner_register_proposal_spec.rb
+++ spec/system/proposal/owner_register_proposal_spec.rb
@@ -134,4 +134,42 @@ describe 'Owner register proposal' do
     expect(page).to have_content 'Data de expiração deve ser futura'
     expect(page).to have_content 'Forma de pagamento não pode ficar em branco'
   end
+
+  xit 'if it is yours' do
+  end
+
+  it 'click on back button' do
+    owner = Owner.create!(name: 'Splinter', email: 'splinter@email.com', password: 'password')
+
+    buffet = Buffet.create!(brand_name: 'Teenage Mutant Ninja Turtles', corporate_name: 'TMNT Splinter LTDA',
+                            registration_number: '88392017000182', phone: '11912341234',
+                            email: 'contato@tmntsplinter.com', address: 'Rua Estados Unidos, 1030 - Jardins',
+                            city: 'São Paulo', state: 'SP', zip_code: '01234123',
+                            description: 'Melhor Buffet da região. Cowabunga', payment: 'PIX, Cartão de Débito', owner:)
+
+    event = Event.create!(name: 'Festa de casamento', description: 'Festa de casamento dos sonhos', min_people: 10,
+                          max_people: 100, duration: 420, menu: 'Pizza', alcoholic_beverages: true, decoration: true,
+                          parking: true, parking_valet: true, customer_space: true, buffet:)
+
+    EventCost.create!(description: 'Dias de semana', minimum: 200_000, additional_per_person: 7_000, overtime: 100_000,
+                      event:)
+
+    customer = Customer.create!(name: 'Dragon Shiryu', cpf: '665.455.630-50', email: 'shiryu@email.com',
+                                password: 'shiryu123')
+
+    future_date = 2.week.from_now.strftime('%d/%m/%Y')
+    order = Order.create!(event_date: future_date, people: 80, details: 'Dia especial', address: 'Sítio', buffet:,
+                          customer:, event:)
+
+    login_as owner, scope: :owner
+    visit root_path
+    within 'nav' do
+      click_on 'Pedidos'
+    end
+    click_on "#{future_date} - #{order.code}"
+    click_on 'Aprovar pedido'
+    click_on 'Voltar para Pedido'
+
+    expect(current_path).to eq order_path(order)
+  end
 end
